10e9c59544d9f8a4c905cae337e22424a5b4f56c
Brian Peacock <bpeacock@signal.co>
2015-04-15 09:53:57 -0500

    Fix side scroll highlighting

--- site/main.js
+++ site/main.js
@@ -34,7 +34,7 @@ $(function() {
   onResize();
 
   // Active Link Selection
-  var path        = window.location.pathname.replace(/^\/LoMein/, ''),
+  var path        = window.location.pathname.replace(/^\/lo-mein/, ''),
       $headings   = $('h2[id]'),
       $asideLinks = $('aside a');
 
